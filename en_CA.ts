12a0dcd3385c7fa69fedb7654805f24fff3e5113
Manuel Barrette <ragnathok@gmail.com>
2019-12-26 19:38:43 -0500

    Translations and prep work / Traductions et préparatifs

---
+++ en_CA.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="fr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Mouvement harmonique</source>
+        <translation>Harmonic motion</translation>
+    </message>
+    <message>
+        <source>Quitter</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <source>Amplitude (m)</source>
+        <translation>Amplitude (m)</translation>
+    </message>
+    <message>
+        <source>Fréquence (Hz)</source>
+        <translation>Frequency (Hz)</translation>
+    </message>
+    <message>
+        <source>Constante de phase (rad)</source>
+        <translation>Phase constant (rad)</translation>
+    </message>
+    <message>
+        <source>Amortissement (Hz)</source>
+        <translation>Damping (Hz)</translation>
+    </message>
+    <message>
+        <source>Position (m)</source>
+        <translation>Position (m)</translation>
+    </message>
+    <message>
+        <source>Temps (s)</source>
+        <translation>Time (s)</translation>
+    </message>
+    <message>
+        <source>Aide</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <source>À propos</source>
+        <translation>About</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>À propos</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <source>Fermer</source>
+        <translation>Close</translation>
+    </message>
+</context>
+</TS>
---
+++ fr_CA.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="fr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Mouvement harmonique</source>
+        <translation>Mouvement harmonique</translation>
+    </message>
+    <message>
+        <source>Quitter</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <source>Amplitude (m)</source>
+        <translation>Amplitude (m)</translation>
+    </message>
+    <message>
+        <source>Fréquence (Hz)</source>
+        <translation>Fréquence (Hz)</translation>
+    </message>
+    <message>
+        <source>Constante de phase (rad)</source>
+        <translation>Constante de phase (rad)</translation>
+    </message>
+    <message>
+        <source>Amortissement (Hz)</source>
+        <translation>Amortissement (Hz)</translation>
+    </message>
+    <message>
+        <source>Position (m)</source>
+        <translation>Position (m)</translation>
+    </message>
+    <message>
+        <source>Temps (s)</source>
+        <translation>Temps (s)</translation>
+    </message>
+    <message>
+        <source>Aide</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <source>À propos</source>
+        <translation>About</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>À propos</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <source>Fermer</source>
+        <translation>Close</translation>
+    </message>
+</context>
+</TS>
